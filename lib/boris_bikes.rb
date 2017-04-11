27a002cac35cfd732b28aa479d8032a9b9f6e40a
Anthony Crisp <anthony_crisp@hotmail.com>
2017-04-11 18:28:08 +0100

    Add BorisBikes challenge 9

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -5,3 +5,11 @@ class DockingStation
   end
 
 end
+
+class Bike
+
+  def working?
+
+  end
+
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,4 @@
-require 'docking_station'
+require 'boris_bikes'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
