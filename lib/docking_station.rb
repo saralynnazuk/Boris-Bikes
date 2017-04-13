677a7c60f9330002ac2aee243668208d55dce721
Sara Riley <saralynnaz@gmail.com>
2017-04-13 15:15:17 +0100

    Add challenges 12-14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
 
-  def release_bike
-    Bike.new
+  def initialize
+    @bike_count = []
   end
 
+  def dock(bike)
+    raise "Dock full!" if @bike_count.count == 20
+    @bike_count << bike
+  end
+
+  def release_bike
+    raise "No more bikes!" unless @bike_count.count > 0
+    @bike_count.pop
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,36 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
+  end
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq [bike]
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq @bike_count
+  end
+
+  describe '#release_bike' do
+
+    it 'gives an error when no more bikes' do
+      expect{ subject.release_bike }.to raise_error "No more bikes!"
+    end
+  end
+
+  describe '#dock' do
+
+    it 'gives an error when dock full' do
+      20.times {subject.dock(Bike.new)}
+      expect{ subject.dock Bike.new }.to raise_error "Dock full!"
+    end
   end
 end
